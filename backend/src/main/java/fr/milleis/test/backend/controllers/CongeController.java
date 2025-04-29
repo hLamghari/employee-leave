b1d466d98c6ad2ccd599081f2fec7df5bf27295b
HLamghari <hatim.lamghari@gmail.com>
2025-04-29 13:11:41 +0200

    fix conge endpoint url mapping

--- backend/src/main/java/fr/milleis/test/backend/controllers/CongeController.java
+++ backend/src/main/java/fr/milleis/test/backend/controllers/CongeController.java
@@ -12,8 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/employes")
 public class CongeController {
 
     private CongeService congeService;
