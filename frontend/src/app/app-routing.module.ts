d3675b8ee9f4940a77cccc4f2a0655f272263ea3
HLamghari <hatim.lamghari@gmail.com>
2025-04-29 13:19:25 +0200

    add routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeListComponent } from './pages/employes/employe-list/employe-list.component';
+import { EmployeDetailComponent } from './pages/employes/employe-detail/employe-detail.component';
+import { DemandeCongeComponent } from './pages/conges/demande-conge/demande-conge.component';
 
 
 const routes: Routes = [
-
+  { path: '', redirectTo: 'employes', pathMatch: 'full' },
+  { path: 'employes', component: EmployeListComponent },
+  { path: 'employes/:id', component: EmployeDetailComponent },
+  { path: 'demande-conge', component: DemandeCongeComponent }
 ];
 
 @NgModule({
@@ -11,3 +17,9 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+<nav>
+  <a routerLink="/employes">Liste des employés</a>
+  <a routerLink="/demande-conge">Demande de congé</a>
+</nav>
+<router-outlet></router-outlet>
