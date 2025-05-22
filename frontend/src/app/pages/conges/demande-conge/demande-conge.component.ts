882c24747ed9a6072326adc81bc8d9109c3770b6
HLamghari <hatim.lamghari@gmail.com>
2025-05-22 17:21:03 +0200

    styling and validation of leave form

--- frontend/src/app/pages/conges/demande-conge/demande-conge.component.ts
+++ frontend/src/app/pages/conges/demande-conge/demande-conge.component.ts
@@ -4,11 +4,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-demande-conge',
-  templateUrl: './demande-conge.component.html'
+  templateUrl: './demande-conge.component.html',
+  styleUrl: './demande-conge.component.css'
 })
 export class DemandeCongeComponent {
   form: FormGroup;
   message = '';
+  error = false;
 
   constructor(private fb: FormBuilder, private congeService: CongeService) {
     this.form = this.fb.group({
@@ -20,13 +22,19 @@ export class DemandeCongeComponent {
   }
 
   submit(): void {
+    console.log('click');
+    
     if (this.form.valid) {
       this.congeService.demanderConge(this.form.value).subscribe({
         next: () => {
           this.message = 'Demande envoyée avec succès !';
+          this.error = false;
           this.init();
         },
-        error: err => this.message =  "Erreur : " + err.error.message || 'Erreur inconnue'
+        error: err => {
+          this.message =  "Erreur : " + err.error.message || 'Erreur inconnue';
+          this.error = true;
+        }
       });
     }
   }
